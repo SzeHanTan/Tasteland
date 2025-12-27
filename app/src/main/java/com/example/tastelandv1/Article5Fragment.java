6dca4e321c58313a5ae44b1f3fd891e1cdda0545
Tang Yvone <yvonetangyvone@gmail.com>
2025-12-28 01:18:48 +0800

    Modify Insight page

---
+++ app/src/main/java/com/example/tastelandv1/Article5Fragment.java
@@ -0,0 +1,22 @@
+package com.example.tastelandv1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Article5Fragment extends Fragment {
+
+    public Article5Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_article_5, container, false);
+    }
+}
